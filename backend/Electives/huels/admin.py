e37a95ec80c2e3b152f3c86cfad040ad6f6032df
Prakhar Mundra <prmundra0802@gmail.com>
2023-01-05 17:09:11 +0530

    Added remaining models

--- backend/Electives/huels/admin.py
+++ backend/Electives/huels/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Courses
+from .models import Courses,Review,SemEntry
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 class CourseResource(resources.ModelResource):
@@ -9,3 +9,5 @@ class CourseAdmin(ImportExportModelAdmin):
    resource_class = CourseResource
 
 admin.site.register(Courses,CourseAdmin)
+admin.site.register(SemEntry)
+admin.site.register(Review)
