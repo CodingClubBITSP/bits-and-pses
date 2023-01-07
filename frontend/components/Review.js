dc819e3f6b76fe64864083962b962e9651db8ffb
Prateek Kashyap <kashyapprateek13@gmail.com>
2023-01-07 14:56:25 +0530

    feat: add hamburger

--- frontend/components/Review.js
+++ frontend/components/Review.js
@@ -1,6 +1,8 @@
 import { useState } from "react";
+
 export default function Review() {
   const [resp, setResp] = useState("neutral");
+  
   return (
     <>
       <div className="bg-[#F9F9F9] w-[100%]  mr-5">
--- frontend/components/Search.js
+++ frontend/components/Search.js
@@ -1,14 +1,24 @@
+import { useState } from "react";
+import { Spin as Hamburger } from "hamburger-react";
+
 export default function Search() {
+  const [open, setOpen] = useState(false);
+  if (open) console.log("yes");
+
   return (
-    <div className="bg-[#E0E0E0] gap-3 p-4 flex flex-col justify-center pt-8">
-      <div className=" text-3xl font-semibold text-[#606060]">Courses</div>
-      <div className="flex items-center justify-between pr-6">
-        <div className="bg-white px-1.5 py-2 rounded-md w-max space-y-1">
-          <div className="w-8 rounded-sm h-1 bg-gray-600"></div>
-          <div className="w-8 rounded-sm h-1 bg-gray-600"></div>
-          <div className="w-8 rounded-sm h-1 bg-gray-600"></div>
+    <div className="bg-[#E0E0E0] gap-5 p-4 flex flex-col justify-center pt-8">
+      <div className="text-3xl font-semibold text-[#606060]">Courses</div>
+      <div className="flex items-stretch gap-4 justify-between pr-6 pl-3">
+        <div className="bg-white rounded-md">
+          <Hamburger
+            rounded
+            color="#4b5563"
+            size={32}
+            distance="sm"
+            onToggle={(toggled) => setOpen(toggled ? true : false)}
+          />
         </div>
-        <input type="text" className="h-9 rounded-md w-[70vw]" />
+        <input type="text" className="p-2 rounded-md w-[70vw]" />
       </div>
     </div>
   );
