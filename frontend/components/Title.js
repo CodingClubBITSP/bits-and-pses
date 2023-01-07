cbdafb8af3f4e01626e5e42e11f1e42ad8e3ea55
Prateek Kashyap <kashyapprateek13@gmail.com>
2023-01-07 14:18:47 +0530

    build: transfer frontend files

---
+++ frontend/components/Title.js
@@ -0,0 +1,12 @@
+export default function Title() {
+  return (
+    <div className="p-4 flex flex-col justify-center pt-8">
+      <div className=" text-3xl font-semibold content-center text-[#606060]">
+        Applied Philosopy
+      </div>
+      <div className=" text-green-600 mt-1 mb-1 font-light">HSS F311</div>
+      <div className=" text-[#8A8A8A] font-light">Taken by: Manoj Kanan</div>
+      <div className="text-[#8A8A8A] font-light">Credits on Completion: 3</div>
+    </div>
+  );
+}
