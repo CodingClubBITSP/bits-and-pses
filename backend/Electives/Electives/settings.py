3ba4acbd5ac266c80ff8544c940cbffd7ebc6691
Ridham1402 <ridhammittal2@gmail.com>
2023-01-09 18:19:35 +0530

    deployment

--- backend/Electives/Electives/settings.py
+++ backend/Electives/Electives/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-tn%7fo#(-p*mx8j#f(=wto_)4mg&_3_ri=z5(6(_wf=e-k_scb
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
 # Default primary key field type
