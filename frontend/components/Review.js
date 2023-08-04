ee9e8c61ec0fbd7c0846277bdcdd1630f2ece65a
OnkarDeshmukh67 <120507028+OnkarDeshmukh67@users.noreply.github.com>
2023-08-04 18:25:39 +0530

    added review section

--- frontend/components/Review.js
+++ frontend/components/Review.js
@@ -1,5 +1,48 @@
 import { useState } from "react";
 
+function makendisplay (s) {
+
+  let nameArr = Object.keys(JSON.parse(s.Reviews))
+  let tipsArr = Object.values(JSON.parse(s.Reviews))
+
+  const obj = nameArr.map((name, index) => {
+    return {key : [index] , Name: name, Tip: tipsArr[index] };
+  });
+
+  // console.log(nameArr)
+  // console.log(tipsArr)
+  // console.log(obj)
+
+  const elem = obj.map( (el) => {
+    return(
+      <>
+             <div className="text-[#2A9134] text-[20px] mx-10 my-2 font-semibold">
+                 {el.Name}
+              </div>
+             <div className="bg-[#e0e0e0] mx-10 my-3 border-[#e0e0e0] border-1 rounded px-5 py-2 text-[14px] text-[#8A8A8A]">
+               {el.Tip}
+             </div>
+         </>
+    )
+  }
+)
+
+return(elem)
+  
+  // return ( 
+  //   <>
+  //       <div className="text-[#2A9134] text-[20px] mx-10 my-2 font-semibold">
+  //           {props.a}
+  //        </div>
+  //        <div className="bg-[#e0e0e0] mx-10 my-3 border-[#e0e0e0] border-1 rounded px-5 py-2 text-[14px] text-[#8A8A8A]">
+  //           {props.b}
+  //       </div>
+  //   </>
+  // )
+
+}
+
+
 export default function Review({ data }) {
   const [selected, setSelected] = useState(1);
   const active =
@@ -12,28 +55,12 @@ export default function Review({ data }) {
       <div className="bg-[#F9F9F9] w-[100%]  mr-5">
         <div className="flex items-center">
           <div
-            className={selected == 1 ? active : other}
-            onClick={() => {
-              setSelected(1);
-            }}
-          >
-            Positive Opn.
-          </div>
-          <div
-            className={selected == 2 ? active : other}
-            onClick={() => {
-              setSelected(2);
-            }}
-          >
-            Neutral Opn.
-          </div>
-          <div
             className={selected == 3 ? active : other}
             onClick={() => {
               setSelected(3);
             }}
           >
-            Negative Opn.
+            General Opn.
           </div>
           <div
             className={selected == 4 ? active : other}
@@ -48,41 +75,12 @@ export default function Review({ data }) {
 
       <div className="lg:flex lg:flex-wrap py-4">
         <div className=" lg:flex lg:flex-col lg:flex-1">
-          <div className="text-[#2A9134] text-[20px] mx-10 my-2 font-semibold">
-            NAME
-          </div>
-          <div className="bg-[#e0e0e0] mx-10 my-3 border-[#e0e0e0] border-1 rounded px-5 py-2 text-[14px] text-[#8A8A8A]">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe esse
-            reprehenderit hic quod. Debitis, aliquam voluptatibus? Dolor
-            asperiores, modi porro quia recusandae repudiandae quod nemo ratione
-            rerum labore, obcaecati laborum?
-          </div>
-        </div>
 
-        <div className=" lg:flex lg:flex-col lg:flex-1">
-          <div className="text-[#2A9134] text-[20px] mx-10 my-2 font-semibold">
-            NAME
-          </div>
-          <div className="bg-[#e0e0e0] mx-10 my-3 border-[#e0e0e0] border-1 rounded px-5 py-2 text-[14px] text-[#8A8A8A]">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe esse
-            reprehenderit hic quod. Debitis, aliquam voluptatibus? Dolor
-            asperiores, modi porro quia recusandae repudiandae quod nemo ratione
-            rerum labore, obcaecati laborum?
-          </div>
-        </div>
+        {data.Reviews ? makendisplay(data) : 'No Review'}
 
-        <div className=" lg:flex lg:flex-col lg:flex-1">
-          <div className="text-[#2A9134] text-[20px] mx-10 my-2 font-semibold">
-            NAME
-          </div>
-          <div className="bg-[#e0e0e0] mx-10 my-3 border-[#e0e0e0] border-1 rounded px-5 py-2 text-[14px] text-[#8A8A8A]">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe esse
-            reprehenderit hic quod. Debitis, aliquam voluptatibus? Dolor
-            asperiores, modi porro quia recusandae repudiandae quod nemo ratione
-            rerum labore, obcaecati laborum?
-          </div>
         </div>
       </div>
     </>
   );
 }
+// Object.keys(JSON.parse(data.Reviews)).map( el => {return <Name a = {el} b = {Object.values(JSON.parse(data.Reviews.replace('/' , '')))} />})
