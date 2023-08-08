5565635ae6346482ca73818bf76983c3c603bb70
Devam-Sheth <devamsheth1203@gmail.com>
2023-08-08 10:59:37 +0530

    changes

--- frontend/components/Description.js
+++ frontend/components/Description.js
@@ -3,8 +3,6 @@ import StarRating from "../components/StarRating";
 import { useState ,  useEffect} from "react";
 import axios from "axios";
 import { useCookies } from "react-cookie";
-
-import React, { useEffect } from "react";
 import { getCourselist, getCodelist, DropdownMenu } from "./getCourse";
 import Link from "next/link";
 
